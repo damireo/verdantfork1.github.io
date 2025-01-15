396b097b7f0b3b3f3e7feb918a3c0de06da2253c
damireo <damiomabuwa177@gmail.com>
2025-01-14 23:20:33 -0600

    Add files via upload

---
+++ verd_fork.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<p id = "mission" ></p>
+
+function ourMission(){
+        
+    document.getElementById("mission").innerHTML = "At Verdant Fork, our mission is to provide sustainable, nourishing meals to all by prioritizing thoughtful resource use and promoting a plant-based cuisine. We are dedicated to minimalistic thinking, inclusivity, and accessible nutritional wellness, striving to create sustainable practices across diverse cultures. Through our focus on energy, water, and dietary sustainability, Verdant Fork aims to create a healthy, inclusive, and accessible menu that celebrates and serves people of all backgrounds."
+}
+
+</html>
